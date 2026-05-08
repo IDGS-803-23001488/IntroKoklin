0168d6a18b2bb2dbc04b7421fafd7e5b323b4435
IDGS-803-23001488 <agtz50538@gmail.com>
2026-05-07 21:00:57 -0600

    07/05/2026. Tipos de datos

---
+++ ejemplo1/temaVariables.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val saludo: String = "Hola" // Inmmutable
+    val nombre: String = "Kotlin" // Mutable
+    println("$saludo $nombre")
+
+
+    // Lectura de datos por consola
+    println("Ingresa primer numero")
+    val num1 = readln().toInt()
+    println("Ingresa segundo numero")
+    val num2 = readln().toInt()
+
+    var res = num1 + num2
+    println("La suma es: $res")
+}
